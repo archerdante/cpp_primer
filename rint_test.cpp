017b7463ff44de2e66d839ab04f5d6d01a68ec99
archerdante <1413282843@qq.com>
2016-03-13 11:29:34 +0800

    test rint function

---
+++ rint_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	cout << rint(1.1) << endl;  //find the nearest intger to a float or double
+	cout << rint(1.8) << endl;
+	cout << rint(-1.1) << endl;
+	cout << rint(-1.8) << endl;
+}
